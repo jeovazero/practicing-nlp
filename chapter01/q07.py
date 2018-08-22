447e1e4b6cbb9e3fe1e89402195517c6aa942723
jeovazero <jeovapereira2000@live.com>
2018-08-22 15:03:06 -0300

    added q07

---
+++ chapter01/q07.py
@@ -0,0 +1,12 @@
+'''
+  Chapter 01   - https://www.nltk.org/book/ch01.html
+
+  Section 08   - Exercises
+
+  Question 07  - Find the collocations in text5.
+
+'''
+from nltk.book import text5
+from nltk.collocations import *
+
+text5.collocations()
