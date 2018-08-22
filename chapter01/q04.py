61aa619d364d453caf0772858ee1daf02d102419
jeovazero <jeovapereira2000@live.com>
2018-08-22 13:14:51 -0300

    q04 updated

--- chapter01/q04.py
+++ chapter01/q04.py
@@ -1,3 +1,9 @@
+'''
+  Chapter 01   - https://www.nltk.org/book/ch01.html
+  Section 08   - Exercises
+  Question 04  - How many words are there in text2? How many distinct words are there?
+'''
+
 from nltk.book import text2
 
 f_words = lambda t: len(t)
